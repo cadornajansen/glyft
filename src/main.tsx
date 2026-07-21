600c1e878aafd001281f9e1a547c72e6163ea68b
Jansen Cadorna <119676423+cadornajansen@users.noreply.github.com>
2026-07-21 17:44:11 +0800

    Install compact canvas selection UI

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,10 @@ import App from "./App.tsx";
 import "./index.css";
 import "./propertiesPanelPolish.css";
 import { installColorInputCoalescing } from "./installColorInputCoalescing";
+import { installCompactSelectionUI } from "./canvas/installCompactSelectionUI";
 
 installColorInputCoalescing();
+installCompactSelectionUI();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
