bd59150625900b18ed36b0e613a11d4ef0716564
Jansen Cadorna <119676423+cadornajansen@users.noreply.github.com>
2026-07-21 18:46:29 +0800

    feat: enable text typography polish

--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,12 @@ import "./propertiesPanelPolish.css";
 import { installColorInputCoalescing } from "./installColorInputCoalescing";
 import { installCompactSelectionUI } from "./canvas/installCompactSelectionUI";
 import { installElementContextMenu } from "./canvas/installElementContextMenu";
+import { installTextPropertyPolish } from "./canvas/installTextPropertyPolish";
 
 installColorInputCoalescing();
 installCompactSelectionUI();
 installElementContextMenu();
+installTextPropertyPolish();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
