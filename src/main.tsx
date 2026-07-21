7eaf211eee2334d2b530127f60cf38bb7401e724
Jansen Cadorna <119676423+cadornajansen@users.noreply.github.com>
2026-07-21 16:22:29 +0800

    feat: load compact properties panel styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import "./propertiesPanelPolish.css";
 import { installColorInputCoalescing } from "./installColorInputCoalescing";
 
 installColorInputCoalescing();
